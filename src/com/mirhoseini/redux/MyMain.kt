f61819dd2294ba99e4aa75544e88211983eabb68
Mohsen <mohsen@germanautolabs.com>
2017-07-24 15:00:25 +0200

    Replace * with Kotlin Any data type

--- src/com/mirhoseini/redux/MyMain.kt
+++ src/com/mirhoseini/redux/MyMain.kt
@@ -4,19 +4,19 @@ package com.mirhoseini.redux
  * Created by Mohsen on 24.07.17.
  */
 
-private var store: Store<Action<CounterAction, *>, State>? = null
+private var store: Store<Action<CounterAction, Any>, State>? = null
 
 fun main(args: Array<String>) {
-    val store = Store<Action<CounterAction, *>, State>(reduce, // reducer function
+    val store = Store(reduce, // reducer function
             State(0), // initial state
-            Logger<Action<CounterAction, *>, State>("Counter"))   // Middleware: logger
+            Logger("Counter"))   // Middleware: logger
 
-    val s1 = store.dispatch(Action<CounterAction, Int>(type = CounterAction.INCREMENT))
-    val s2 = store.dispatch(Action<CounterAction, Int>(type = CounterAction.PLUS, value = 10))
+    val s1 = store.dispatch(Action(type = CounterAction.INCREMENT))
+    val s2 = store.dispatch(Action(type = CounterAction.PLUS, value = 10))
 }
 
-object reduce : Store.Reducer<Action<CounterAction, *>, State> {
-    override fun reduce(action: Action<CounterAction, *>, currentState: State): State {
+object reduce : Store.Reducer<Action<CounterAction, Any>, State> {
+    override fun reduce(action: Action<CounterAction, Any>, currentState: State): State {
         when (action.type) {
             CounterAction.INCREMENT -> return State(currentState.count + 1)
             CounterAction.PLUS -> return State(currentState.count + action.value as Int)
